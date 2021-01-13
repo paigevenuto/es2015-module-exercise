56a3ed43c65681accc5b2afca6de3c2b91d08163
paigevenuto <paigevenuto@gmail.com>
2021-01-12 22:07:54 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import foods from './foods'
+import {choice, remove} from './helpers'
+
+const randFruit = choice(foods)
+console.log(`I’d like one ${randFruit}, please`)
+console.log(`Here you go: ${randFruit}`)
+console.log('Delicious! May I have another?')
+remove(foods, randFruit)
+console.log(`I’m sorry, we’re all out. We have ${foods.length} left.`)
